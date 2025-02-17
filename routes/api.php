90b2928818ab712d601a79b38dfec60623da7f7a
ghimirelava <lghimire@usfca.edu>
2025-02-17 11:25:09 -0800

    writing multi variabled json reponse

--- routes/api.php
+++ routes/api.php
@@ -3,14 +3,31 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// route listens to for a get request to the root URL and if it receives one, it returns a JSON response with a message.
+// route listens to for a get request to the root URL and if it receives one, it returns a JSON response with a phone number, queues, status, and welcome text.
 Route::get('/', function() {
     return response()->json([
-        'message' => 'Hello, API!'
+        'phone_number' => '1234567890',
+        'queues' => [
+            [
+                //queue id
+                'id' => 'q1',
+                //queue name
+                'queue_name' => 'Family Medicine',
+            ],
+            [
+                'id' => 'q2',
+                'queue_name' => 'Pediatrics',
+            ],
+            [
+                'id' => 'q3',
+                'queue_name' => 'Dermatology',
+            ]
+        ],
+        'status' => true,
+        'welcome_text' => 'Welcome to the LA Hospital.'
     ], 200);
 
 });
-
 
 //route handles user endpoint
 Route::get('/user', function (Request $request) {
