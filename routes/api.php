dfb79de14869a4621d097b466b38420b15dc073f
ghimirelava <lghimire@usfca.edu>
2025-02-16 16:41:24 -0800

    comments added to understand routes

--- routes/api.php
+++ routes/api.php
@@ -3,14 +3,16 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// route handles root endpoint
+// route listens to for a get request to the root URL and if it receives one, it returns a JSON response with a message.
 Route::get('/', function() {
     return response()->json([
         'message' => 'Hello, API!'
     ], 200);
+
 });
 
+
 //route handles user endpoint
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::get('/user', function (Request $request) {
     return $request->user();
-});
+})->middleware('auth:sanctum');
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome'); //resources/views/welcome.blade.php
+    //return "Hello, Laravel!"; //used to test database connection issue 
 });
